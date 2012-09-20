87717cb3ddbeafe6130f3a495a4f5c2f881079f2
Changyou Sun <csun@cfr.msstate.edu>
2012-09-20 06:42:29 +0000

    version 1.2

--- R/print.ecm.R
+++ R/print.ecm.R
@@ -1,27 +1,20 @@
-print.ecm <-
-function (x,...)
+print.ecm <- function(x, ...)
 {
-    if (!(class(x)[1] == "ecm")) {
-        stop("\n Need an object of class 'ecm' from a function of ecmSymFit or ecmAsyFit.\n")}
+  if (class(x)[2] == "ecmSymFit") {
+      ti="ECM - Symmetric + linear cointegration - "
+  } else {
+      if (class(x)[2] == "ecmAsyFit" & x$model == "linear") {
+          ti="ECM - Asymmetric + linear cointegration - "
+      } else {ti="ECM - Asymmetric + nonlinear threshold cointegration - "}
+  }
 
-    if (class(x)[2] == "ecmSymFit") {
-        ti="ECM - Symmetric + linear cointegration - "
-    } else {
-        if (class(x)[2] == "ecmAsyFit" & x$model == "linear") {
-            ti="ECM - Asymmetric + linear cointegration - "
-        } else {
-            ti="ECM - Asymmetric + nonlinear threshold cointegration - "        
-        }
-    }
-
-    cat("\n===============================================================")
-    cat(paste("\n", ti, " \"", x$name.x, "\"", "\n", sep=""))       
-    cat("===============================================================\n")       
-    print(summary(x$ecm.x))
-    
-    cat("\n===============================================================")
-    cat(paste("\n", ti, " \"", x$name.y, "\"", "\n", sep=""))       
-    cat("===============================================================\n")       
-    print(summary(x$ecm.y))
-}
-
+  cat("\n===============================================================")
+  cat(paste("\n", ti, " \"", x$name.x, "\"", "\n", sep=""))       
+  cat("===============================================================\n")       
+  print(summary(x$ecm.x))
+  
+  cat("\n===============================================================")
+  cat(paste("\n", ti, " \"", x$name.y, "\"", "\n", sep=""))       
+  cat("===============================================================\n")       
+  print(summary(x$ecm.y))
+} 
